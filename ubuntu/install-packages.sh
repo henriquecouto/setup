cf54ad5fad68143b11a54c8d27740dd4fb1f0b83
Henrique Couto <henrique.coutu@live.com>
2020-02-27 21:19:03 -0300

    uncomment nvm bash install

--- ubuntu/install-packages.sh
+++ ubuntu/install-packages.sh
@@ -33,7 +33,7 @@ echo "INSTALLING FISH THEME"
 fish -c "omf install spacefish"
 
 echo "INSTALLING NVM"
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 fish -c "omf install nvm"
 
 echo "INSTALLING GOOGLE CHROME"
