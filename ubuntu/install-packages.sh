10e956d570cbf9cf9bca080334127b7b90d05dac
Henrique Couto <henrique.coutu@live.com>
2020-03-03 20:32:38 -0300

    added docker and docker compose

--- ubuntu/install-packages.sh
+++ ubuntu/install-packages.sh
@@ -26,6 +26,19 @@ sudo yarn global add create-react-app
 echo "INSTALL FIREBASE TOOLS"
 sudo yarn global add firebase-tools
 
+echo "INSTALL DOCKER"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+echo "INSTALL DOCKER COMPOSE"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 echo "INSTALLING OH MY FISH"
 fish -c "curl -L https://get.oh-my.fish | fish"
 
