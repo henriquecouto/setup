1b4243814ee29135ae9ca9d91401151cd1c3d45e
Henrique Couto <henrique.coutu@live.com>
2020-03-05 15:52:15 -0300

    docker update

--- ubuntu/install-packages.sh
+++ ubuntu/install-packages.sh
@@ -31,8 +31,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker $USER
-newgrp docker
 
 echo "INSTALL DOCKER COMPOSE"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
